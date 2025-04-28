fa120d82abad926dad04d4146431657d9eeecca7
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2025-04-28 20:36:18 +0300

    feat: update button styles

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -5,7 +5,11 @@ interface ButtonProps {
 
 const Button: React.FC<ButtonProps> = ({ text, onClick }) => {
   return (
-    <button className="button" type="button" onClick={onClick}>
+    <button
+      className="cursor-pointer rounded border border-gray-400 bg-white px-4 py-2 font-semibold text-gray-800 shadow select-none hover:bg-gray-100"
+      type="button"
+      onClick={onClick}
+    >
       {text}
     </button>
   );
