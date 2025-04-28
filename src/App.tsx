ee917a19417ceb93e127af29b7e100a8d83f85e0
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2025-04-28 20:55:29 +0300

    feat: update App component

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { useState } from 'react';
 import styles from './App.module.css';
 import ReactLogo from './assets/icons/react.svg';
 
+const buttonText = 'Decrement';
+
 function App() {
   const [count, setCount] = useState(0);
 
@@ -11,7 +13,7 @@ function App() {
     <div className={styles.container}>
       <ReactLogo onClick={() => setCount((count) => count + 1)} className={styles.logo} />
       <h1 className={`${styles.count} text-5xl`}>{count}</h1>
-      <Button text="Decrement" onClick={() => setCount((count) => count - 1)} />
+      <Button text={buttonText} onClick={() => setCount((count) => count - 1)} />
     </div>
   );
 }
