f209d6f010cde32df82d8f32e4335f5dae1dc358
Rostislav Zhuk <zzzhuchok@gmail.com>
2025-04-30 22:20:52 +0300

    fix: delete temporary test items

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,4 @@
-import { CartPage } from '@pages/CartPage';
-import { NotFoundPage } from '@pages/NotFoundPage';
 import App from 'App';
 import { createBrowserRouter } from 'react-router';
 
-export const router = createBrowserRouter([
-  { path: '/', Component: App, ErrorBoundary: NotFoundPage },
-  { path: 'cart', Component: CartPage },
-]);
+export const router = createBrowserRouter([{ path: '/', Component: App }]);
