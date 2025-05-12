d791ec29da95ddc53380b0fa377cdcea2ffddef1
Aleksei Zhuchkov <kelzzzer@gmail.com>
2025-05-12 16:23:10 +0300

    feat: redirection for sign-in and registration page

--- src/pages/RegistrationPage/RegistrationPage.tsx
+++ src/pages/RegistrationPage/RegistrationPage.tsx
@@ -1,6 +1,14 @@
 import { RegistrationForm } from '@components';
+import { UrlPath } from '@ts-enums';
+import { CustomerContext } from 'context/customer.context';
+import { useContext } from 'react';
+import { useNavigate } from 'react-router';
 
 export const RegistrationPage = () => {
+  const navigate = useNavigate();
+  const { currentCustomer } = useContext(CustomerContext)!;
+  if (currentCustomer) void navigate(UrlPath.HOME);
+
   return (
     <div>
       Registration page
--- src/pages/SignInPage/SignInPage.tsx
+++ src/pages/SignInPage/SignInPage.tsx
@@ -1,6 +1,14 @@
 import { SignInForm } from '@components';
+import { UrlPath } from '@ts-enums';
+import { CustomerContext } from 'context/customer.context';
+import { useContext } from 'react';
+import { useNavigate } from 'react-router';
 
 export const SignInPage = () => {
+  const navigate = useNavigate();
+  const { currentCustomer } = useContext(CustomerContext)!;
+  if (currentCustomer) void navigate(UrlPath.HOME);
+
   return (
     <div>
       Sign-in page
