7155a355cd0c9835e1893233beb3d21753dcf9e5
Aleksei Zhuchkov <kelzzzer@gmail.com>
2025-05-12 09:55:54 +0300

    feat: complete save and load token from localstorage

--- src/services/in-memory-token-cache.ts
+++ src/services/in-memory-token-cache.ts
@@ -1,11 +1,25 @@
 import { type TokenCache, type TokenStore } from '@commercetools/ts-client';
+import { isTokenStore } from '@utils/is-token-store';
+
+const defaultTokenStore = {
+  expirationTime: 0,
+  refreshToken: undefined,
+  token: '',
+};
 
 export class InMemoryTokenCache implements TokenCache {
-  private myCache: TokenStore = {
-    expirationTime: 0,
-    refreshToken: undefined,
-    token: '',
-  };
+  private myCache: TokenStore;
+
+  constructor() {
+    this.myCache = defaultTokenStore;
+    const localStorageToken = localStorage.getItem('token');
+    if (localStorageToken) {
+      const parseToken: unknown = JSON.parse(localStorageToken);
+      if (isTokenStore(parseToken) && parseToken && parseToken.expirationTime < Date.now()) {
+        this.myCache = parseToken;
+      }
+    }
+  }
 
   public get(): TokenStore {
     return this.myCache;
@@ -18,5 +32,7 @@ export class InMemoryTokenCache implements TokenCache {
 
   public set(newCache: TokenStore): void {
     this.myCache = newCache;
+    localStorage.setItem('token', JSON.stringify(this.myCache));
+    console.log(this.myCache);
   }
 }
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,4 @@
 export { registrationAction } from './actions-for-forms/registration-action';
 export { signInAction } from './actions-for-forms/sign-in-action';
 export { convertFormDataToString } from './convert-form-data-to-string';
+export { isTokenStore } from './is-token-store';
