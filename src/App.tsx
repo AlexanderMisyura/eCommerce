f1f3277b3e0e6ea66b95fab5c5e67b0fb9b4d67d
Rostislav Zhuk <zzzhuchok@gmail.com>
2025-05-13 22:01:37 +0300

    feat: add theme and cssbaseline

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { Navigation } from '@components';
+import { ThemeProvider } from '@emotion/react';
+import CssBaseline from '@mui/material/CssBaseline';
 import { Outlet } from 'react-router';
+import { theme } from 'theme';
 
 function App() {
   return (
     <>
-      <Navigation />
-      <Outlet />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Navigation />
+        <Outlet />
+      </ThemeProvider>
     </>
   );
 }
