e4f8a6b2319c565da7c9bc668e0259bb1c6f3a43
Rostislav Zhuk <zzzhuchok@gmail.com>
2025-05-14 21:53:37 +0300

    feat: add LogoLink component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 export { Footer } from './Footer/Footer';
 export { Header } from './Header/Header';
+export { LogoLink } from './LogoLink/LogoLink';
 export { Navigation } from './Navigation/Navigation';
 export { RegistrationForm } from './RegistrationForm/RegistrationForm';
 export { SignInForm } from './SignInForm/SignInForm';
