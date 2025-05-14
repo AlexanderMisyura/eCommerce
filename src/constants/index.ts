d69b2282637f145eaf4163b57cece059a0c516b6
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2025-05-14 05:22:37 +0300

    feat: add debounce wrapper

---
+++ src/constants/index.ts
@@ -0,0 +1,1 @@
+export { DEBOUNCE_TIMEOUT } from './debounce-timeout';
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,5 @@
 export { registrationAction } from './actions-for-forms/registration-action';
 export { signInAction } from './actions-for-forms/sign-in-action';
 export { convertFormDataToString } from './convert-form-data-to-string';
+export { eventDebounceWrapper } from './debounce-wrapper';
 export { isTokenStore } from './is-token-store';
