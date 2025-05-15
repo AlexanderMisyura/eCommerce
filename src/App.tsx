d80ce0141844b7d9579b30b6abddfb0d837b7c1c
zzzhuchok <zzzhuchok@gmail.com>
2025-05-15 16:21:31 +0300

    refactor: simplify App.tsx and improve structure of main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 import { Footer, Header } from '@components';
-import { ThemeProvider } from '@emotion/react';
 import { Container } from '@mui/material';
-import CssBaseline from '@mui/material/CssBaseline';
 import { Outlet } from 'react-router';
-import { theme } from 'theme';
 
 function App() {
   return (
-    <>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <div className="flex min-h-screen flex-col">
-          <Header />
-          <main className="grow">
-            <Container>
-              <Outlet />
-            </Container>
-          </main>
-          <Footer />
-        </div>
-      </ThemeProvider>
-    </>
+    <div className="page flex min-h-screen flex-col">
+      <Header />
+      <main className="grow py-4">
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
+      <Footer />
+    </div>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,28 +3,26 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import './styles/global.css';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
 
-import { CssBaseline } from '@mui/material';
+import { CssBaseline, ThemeProvider } from '@mui/material';
 import { CustomerProvider } from 'context/customer.context';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router';
 import { router } from 'router';
+import { theme } from 'theme';
 
 const root = document.createElement('div');
 root.id = 'root';
-root.classList.add('flex', 'flex-col', 'min-h-screen');
 document.body.append(root);
 
 createRoot(root).render(
   <StrictMode>
-    <CustomerProvider>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
-      <RouterProvider router={router} />
-    </CustomerProvider>
+      <CustomerProvider>
+        <RouterProvider router={router} />
+      </CustomerProvider>
+    </ThemeProvider>
   </StrictMode>
 );
