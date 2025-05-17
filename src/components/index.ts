f833a4929ec8ce861f475b1bb184eebbd565dd0c
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2025-05-17 06:20:45 +0300

    feat: add Confirm component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 export { Navigation } from './Navigation/Navigation';
 export { Addresses } from './RegistrationForm/Addresses/Addresses';
+export { Confirm } from './RegistrationForm/Confirm/Confirm';
 export { Credentials } from './RegistrationForm/Credentials/Credentials';
 export { RegistrationForm } from './RegistrationForm/RegistrationForm';
 export { SignInForm } from './SignInForm/SignInForm';
