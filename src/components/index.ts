bdc0e4fd8e9f0f31060ef12beed37bab9ca4f67c
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2025-05-17 06:18:02 +0300

    feat: add Credentials component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { Navigation } from './Navigation/Navigation';
+export { Credentials } from './RegistrationForm/Credentials/Credentials';
 export { RegistrationForm } from './RegistrationForm/RegistrationForm';
 export { SignInForm } from './SignInForm/SignInForm';
