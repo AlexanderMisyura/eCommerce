70deb651301f1464342a338207899b0845b39330
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2025-05-25 03:48:19 +0300

    refactor(interfaces): move registration and toast context interfaces to context directory

--- src/models/interfaces/index.ts
+++ src/models/interfaces/index.ts
@@ -1,14 +1,15 @@
 export type { CustomerContextType } from './context/customer-context';
+export type {
+  RegistrationContextData,
+  RegistrationContextType,
+} from './context/registration-context';
+export type { ToastContextType } from './context/toast-context';
 export type { Address } from './registration/address';
 export type { Addresses } from './registration/addresses';
 export type { AddressesOptions } from './registration/addresses-options';
 export type { AddressesState } from './registration/addresses-state';
 export type { Credentials } from './registration/credentials';
 export type { CredentialsState } from './registration/credentials-state';
-export type {
-  RegistrationContextData,
-  RegistrationContextType,
-} from './registration/registration-context-type';
 export type { RegistrationData } from './registration/registration-data';
 export type { RegistrationFieldState } from './registration/registration-field-state';
 export type {
@@ -18,4 +19,3 @@ export type {
 export type { StepperProps } from './registration/stepper-props';
 export type { SignInData } from './sign-in/sign-in-data';
 export type { SignInValidationErrors } from './sign-in/sign-in-validation-errors';
-export type { ToastContextType } from './toast-context-type';
