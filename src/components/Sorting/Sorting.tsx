3188dcc9561e255eabbebf359985701689965986
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2025-05-26 03:25:09 +0300

    fix: fix sorting component's navigation, improve styles

--- src/components/Sorting/Sorting.tsx
+++ src/components/Sorting/Sorting.tsx
@@ -1,9 +1,7 @@
 import { SORT_OPTIONS } from '@constants';
 import FormControl from '@mui/material/FormControl';
 import MenuItem from '@mui/material/MenuItem';
-import type { SelectChangeEvent } from '@mui/material/Select';
-import Select from '@mui/material/Select';
-import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
 import { useLocation, useNavigate, useSearchParams } from 'react-router';
 
 export const Sorting: React.FC = () => {
@@ -11,29 +9,34 @@ export const Sorting: React.FC = () => {
   const location = useLocation();
   const [searchParams] = useSearchParams();
 
-  const handleChange = (event: SelectChangeEvent<string>): void => {
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
     const newSearchParams = new URLSearchParams(searchParams);
     const newSortingValue = event.target.value;
 
-    if (newSortingValue === '') {
+    if (newSortingValue === SORT_OPTIONS.DEFAULT) {
       newSearchParams.delete('sort');
     } else {
       newSearchParams.set('sort', newSortingValue);
     }
 
-    void navigate(`${location.pathname}?${searchParams.toString()}`);
+    void navigate(`${location.pathname}?${newSearchParams.toString()}`);
   };
 
   return (
     <FormControl>
-      <Typography>Sort by:</Typography>
-      <Select value={searchParams.get('sort') ?? ''} onChange={handleChange}>
+      <TextField
+        select
+        size="small"
+        label="Sort by:"
+        value={searchParams.get('sort') ?? SORT_OPTIONS.DEFAULT}
+        onChange={handleChange}
+      >
         <MenuItem value={SORT_OPTIONS.DEFAULT}>Default Order</MenuItem>
         <MenuItem value={SORT_OPTIONS.PRICE_ASC}>Price: Low to High</MenuItem>
         <MenuItem value={SORT_OPTIONS.PRICE_DESC}>Price: High to Low</MenuItem>
         <MenuItem value={SORT_OPTIONS.NAME_ASC}>Name: A-Z</MenuItem>
         <MenuItem value={SORT_OPTIONS.NAME_DESC}>Name: Z-A</MenuItem>
-      </Select>
+      </TextField>
     </FormControl>
   );
 };
