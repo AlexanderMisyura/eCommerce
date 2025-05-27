67ca0514bc8d235494d61e74b75582e4f78a5d42
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2025-05-27 04:25:16 +0300

    feat(components): update Sorting and Pagination components, improve styling

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,12 +1,15 @@
 import { CARDS_PER_PAGE_LIMIT } from '@constants';
-import { Pagination as MuiPagination, PaginationItem } from '@mui/material';
-import { Link, useLocation, useSearchParams } from 'react-router';
+import { Box, Pagination as MuiPagination, PaginationItem } from '@mui/material';
+import { Link, useLocation, useNavigation, useSearchParams } from 'react-router';
 
 interface PaginationProps {
   productsTotal: number;
 }
 
 export const Pagination: React.FC<PaginationProps> = ({ productsTotal }) => {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === 'loading';
+
   const [searchParams] = useSearchParams();
   const location = useLocation();
 
@@ -17,20 +20,26 @@ export const Pagination: React.FC<PaginationProps> = ({ productsTotal }) => {
   }
 
   return (
-    <MuiPagination
-      count={totalPages}
-      page={Number(searchParams.get('page')) || 1}
-      renderItem={(item) => {
-        const newSearchParams = new URLSearchParams(searchParams);
-        newSearchParams.set('page', String(item.page));
-        return (
-          <PaginationItem
-            component={Link}
-            to={`${location.pathname}?${newSearchParams.toString()}`}
-            {...item}
-          />
-        );
-      }}
-    />
+    <Box display="flex" justifyContent="center">
+      <MuiPagination
+        size="large"
+        color="primary"
+        count={totalPages}
+        page={Number(searchParams.get('page')) || 1}
+        renderItem={(item) => {
+          const newSearchParams = new URLSearchParams(searchParams);
+          newSearchParams.set('page', String(item.page));
+          return (
+            <PaginationItem
+              component={Link}
+              viewTransition
+              to={`${location.pathname}?${newSearchParams.toString()}`}
+              {...item}
+            />
+          );
+        }}
+        disabled={isLoading}
+      />
+    </Box>
   );
 };
--- src/components/Sorting/Sorting.tsx
+++ src/components/Sorting/Sorting.tsx
@@ -2,9 +2,12 @@ import { SORT_OPTIONS } from '@constants';
 import FormControl from '@mui/material/FormControl';
 import MenuItem from '@mui/material/MenuItem';
 import TextField from '@mui/material/TextField';
-import { useLocation, useNavigate, useSearchParams } from 'react-router';
+import { useLocation, useNavigate, useNavigation, useSearchParams } from 'react-router';
 
 export const Sorting: React.FC = () => {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === 'loading';
+
   const navigate = useNavigate();
   const location = useLocation();
   const [searchParams] = useSearchParams();
@@ -23,13 +26,16 @@ export const Sorting: React.FC = () => {
   };
 
   return (
-    <FormControl>
+    <FormControl sx={{ marginBottom: 2 }}>
       <TextField
         select
         size="small"
+        id="sort"
+        name="sort"
         label="Sort by:"
         value={searchParams.get('sort') ?? SORT_OPTIONS.DEFAULT}
         onChange={handleChange}
+        disabled={isLoading}
       >
         <MenuItem value={SORT_OPTIONS.DEFAULT}>Default Order</MenuItem>
         <MenuItem value={SORT_OPTIONS.PRICE_ASC}>Price: Low to High</MenuItem>
