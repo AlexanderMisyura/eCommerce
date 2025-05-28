93e500f08b86a8a741672e71c308b49b97f97e80
zzzhuchok <zzzhuchok@gmail.com>
2025-05-28 12:41:52 +0300

    refactor(loader): replace Skeleton with Loader in InfoPanel

--- src/components/UserProfile/UserProfileInfoPanel.tsx
+++ src/components/UserProfile/UserProfileInfoPanel.tsx
@@ -2,11 +2,10 @@ import { useCustomerContext } from '@hooks';
 import { useTheme } from '@mui/material';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
+import { LegoLoader } from 'components/Loaders/LegoLoader';
 import { BREADCRUMBS_NAME_MAP } from 'constants/breadcrumbs-name-map';
 import type { ReactNode } from 'react';
 import { useLocation } from 'react-router';
-
-import { UserProfileSkeleton } from './UserProfileSkeleton';
 
 interface UserProfileInfoPanelProps {
   children: ReactNode;
@@ -22,29 +21,24 @@ export const UserProfileInfoPanel: React.FC<UserProfileInfoPanelProps> = ({ chil
 
   return (
     <Stack sx={{ flexGrow: 1, minHeight: '500px' }}>
-      {loading ? (
-        <UserProfileSkeleton />
-      ) : (
-        <>
-          <Typography variant="h3" sx={{ marginBottom: 4 }}>
-            {BREADCRUMBS_NAME_MAP[title]}
-          </Typography>
+      <Typography variant="h3" sx={{ marginBottom: 4 }}>
+        {BREADCRUMBS_NAME_MAP[title]}
+      </Typography>
 
-          <Stack
-            sx={{
-              flexGrow: 1,
-              justifyContent: 'center',
-              alignItems: 'center',
-              border: `1px solid ${palette.grey[400]}`,
-              padding: 4,
-              borderRadius: '10px',
-              backgroundColor: palette.common.white,
-            }}
-          >
-            {children}
-          </Stack>
-        </>
-      )}
+      <Stack
+        sx={{
+          flexGrow: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+          border: `1px solid ${palette.grey[400]}`,
+          padding: 4,
+          borderRadius: '10px',
+          backgroundColor: palette.common.white,
+          position: 'relative',
+        }}
+      >
+        {loading ? <LegoLoader /> : children}
+      </Stack>
     </Stack>
   );
 };
