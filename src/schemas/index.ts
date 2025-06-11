f039101c47b0a279dafe43d27452d9995724bc46
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2025-06-11 21:03:58 +0300

    feat(schemas): add cart update schemas

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,3 +1,4 @@
+export { CartUpdateSchema } from './cart-update-schema';
 export { NumberOfPiecesSchema, RecommendedAgeSchema } from './product-attribute-schema';
 export { ProductProjectionSchema, ProductResponseSchema } from './product-response-schema';
 export { RegistrationSchema } from './registration-schema';
