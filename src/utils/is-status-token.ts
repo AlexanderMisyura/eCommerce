26bf541bfa37abde8fc7d38d1396ce7357a0276f
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2025-06-16 13:42:56 +0300

    feat: remove unused isStatusToken util function

--- src/utils/is-status-token.ts
+++
@@ -1,5 +0,0 @@
-import type { StatusToken } from '@ts-types';
-
-export const isStatusToken = (data: unknown): data is StatusToken => {
-  return data && typeof data === 'string' && (data === 'auth' || data === 'noAuth') ? true : false;
-};
